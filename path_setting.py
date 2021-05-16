a97235a2d0c567fbd9cf51418caa2cab0d8508a9
SunTsai <hldeepy12@gmail.com>
2021-05-16 15:51:20 +0800

    update

--- path_setting.py
+++ path_setting.py
@@ -4,5 +4,4 @@ package_root = os.path.dirname(__file__)
 
 log_dir = os.path.join(package_root, 'log')
 
-page_dir = os.path.join(package_root, 'page')
 keyword_dir = os.path.join(package_root, 'keyword')
